52a104d789aae652287c0eacbd9554ef9dd2eb28
rinal <rinalditoryanari@gmail.com>
2025-03-04 20:35:27 +0700

    add modular axios

---
+++ src/utilities/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+  maxBodyLength: Infinity,
+  baseURL: process.env.VUE_APP_BACKEND_URL + '/api', // Adjust to match your backend URL
+  headers: { 'Content-Type': 'application/json' }
+  //   withCredentials: true // Include cookies with every request
+})
+
+// Handle 401 errors globally
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/auth/login' // Redirect to login on unauthorized error
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
